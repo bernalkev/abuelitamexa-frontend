0018380e150127234f3c1ac7175fffb583e4ac11
Kevin B <>
2024-06-30 11:22:00 -0700

    Abuelita Mexa project - 1st template of landing page with different menu items, navigation bar, social icons, video, image elements, applying flexbox , buttons, text, forms etc.

---
+++ Abuelita_Mexa/videoparts.js
@@ -0,0 +1,24 @@
+<script>
+    const slide = document.querySelector('.carousel-slide');
+    const prevButton = document.querySelector('.carousel-button.prev');
+    const nextButton = document.querySelector('.carousel-button.next');
+    let currentIndex = 0;
+
+    function updateCarousel() {
+        slide.style.transform = `translateX(-${currentIndex * 100}%)`;
+    }
+
+    prevButton.addEventListener('click', () => {
+        if (currentIndex > 0) {
+            currentIndex--;
+            updateCarousel();
+        }
+    });
+
+    nextButton.addEventListener('click', () => {
+        if (currentIndex < slide.children.length - 1) {
+            currentIndex++;
+            updateCarousel();
+        }
+    });
+</script>
